16dfaf8944ae5082fe814a750f9988f324ce2a20
jumperhe <54824920+jumperhe@users.noreply.github.com>
2021-10-27 17:57:35 +0800

    上传了文件
    第一次使用GitHub上传文件

---
+++ 1变量_1.py
@@ -0,0 +1,21 @@
+'''
+1.字母，数字，下划线
+2.不能数字开头
+3. 不能使用关键字
+4.严格区分大小写
+'''
+
+a = 200
+b = 19
+c = 90
+A = 100
+print(a)
+# 名字 = 'he'
+# print(名字)
+
+kl = 'heweijiang'
+ke = '华为'
+print(ke)
+
+list1 = range(2, 19)
+print(type(list1))
